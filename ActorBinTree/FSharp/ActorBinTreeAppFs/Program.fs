14b99fcd490c3daf6d718032cbc687b55332d748
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-08-26 20:26:21 +0100

    Started work on the F# version of ActorBinTree

---
+++ ActorBinTree/FSharp/ActorBinTreeAppFs/Program.fs
@@ -0,0 +1,16 @@
+﻿open Akka.Actor
+open Akka.FSharp
+open ActorBinTreeFs.BinaryTreeSet
+
+[<EntryPoint>]
+let main _ = 
+
+    aref <! Insert (aref, 1, 1)
+    aref <! Contains (aref, 1, 1)
+    aref <! Remove (aref, 1, 1)
+    aref <! GC
+
+    System.Threading.Thread.Sleep(500)
+
+    system.Shutdown()
+    0
---
+++ ActorBinTree/FSharp/ActorBinTreeFs/BinaryTreeSet.fs
@@ -0,0 +1,27 @@
+﻿module ActorBinTreeFs.BinaryTreeSet
+
+open Akka.Actor
+open Akka.FSharp
+
+let config = Akka.FSharp.Configuration.defaultConfig()
+let system = System.create "my-system" config
+
+type BinaryTreeSetMessage =
+    | Insert of IActorRef * int * int
+    | Contains of IActorRef * int * int
+    | Remove of IActorRef * int * int
+    | GC
+
+type BinaryTreeSetMessageReply =
+    | ContainsResult of int * bool
+    | OperationFinished of int
+
+let handleMessage (mailbox: Actor<BinaryTreeSetMessage>) msg = 
+    match msg with
+    | Insert (r, id, e) -> printfn "Insert %d" e
+    | Contains (r, id, e) -> printfn "Contains %d" e
+    | Remove (r, id, e) -> printfn "Remove %d" e
+    | GC -> printfn "GC"
+
+let aref = spawn system "my-actor" (actorOf2 handleMessage)
+let aref2 = spawn system "black-hole" (actorOf (fun _ -> ()))
